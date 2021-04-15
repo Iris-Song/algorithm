3a65d93ac43108477936cfe22292a7c47dbb0f11
Iris <58033867+Iris-Song@users.noreply.github.com>
2021-04-15 08:29:53 +0800

    Create bag.cpp

---
+++ Greedy/bag.cpp
@@ -0,0 +1,20 @@
+//每个物品可以部分装进去
+template<class Type>
+//n个物品，M大小的容量，i号物品的价值为v[i],装入的百分比为x[i]
+//假设已经按照v[i]/w[i]，由大到小的顺序将v[i],w[i]排好序
+void Knapsack(int n,Type M,Type v[],Type w[],Type x[]) 
+{
+	for (int i = 1; i <= n; i++) 
+		x[i] = 0;
+	for (int i = 1; i <= n; i++) {
+		if (w[i] < M) {
+			x[i] = 1;
+			M -= w[i];
+		}
+		else 
+			break;
+	}
+	if (i <= n) {
+		x[i] = M / w[i];
+	}
+}
